6fdb6a389cbb8cbdfd5c2fc94403d28bff82a907
soumya-ranjan-samal <soumyaranjan1405@gmail.com>
2025-12-16 12:56:45 +0530

    Added a functionality

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+const port = 3000;
+const app = express();
+
+app.get('/', (req,res)=>{
+    let now = new Date();
+    let time = now.toLocaleTimeString().split(':')[0];
+    if(time < 12 ){
+        res.send('Good Morning...');
+    }else if(time >= 12 && time < 16 ){
+        res.send('Good Afternoon...');
+    }else if(time >= 16 && time < 24){
+        res.send('Good Evening...');
+    }else{
+        res.send('Hello dear...');
+    }
+});
+
+app.listen(port, ()=>{
+    console.log('Server Statred Listening...');
+});
